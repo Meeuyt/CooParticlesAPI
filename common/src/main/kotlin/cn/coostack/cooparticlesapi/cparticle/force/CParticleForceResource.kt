79a965f95ca71b61f564531d36ec7e1191a7ec84
Meeuyt <meeut@example.com>
2026-09-20 17:47:33 +0800

    fix(common): make CParticleForceResource open for extension

--- common/src/main/kotlin/cn/coostack/cooparticlesapi/cparticle/force/CParticleForceResource.kt
+++ common/src/main/kotlin/cn/coostack/cooparticlesapi/cparticle/force/CParticleForceResource.kt
@@ -1,6 +1,6 @@
 package cn.coostack.cooparticlesapi.cparticle.force
 
-class CParticleForceResource(val id: String) {
+open class CParticleForceResource(val id: String) {
     override fun equals(other: Any?): Boolean = other is CParticleForceResource && other.id == id
     override fun hashCode(): Int = id.hashCode()
 }
